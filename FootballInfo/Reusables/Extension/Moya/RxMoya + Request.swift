4d58f945405a597734ced18715c5f27a53ab9a8a
Fathureza Januarza <fathureza.januarza@icehousecorp.com>
2018-12-08 21:29:59 +0700

    Configure method for requestWithValidation

--- FootballInfo/Reusables/Extension/Moya/RxMoya + Request.swift
+++ FootballInfo/Reusables/Extension/Moya/RxMoya + Request.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import Moya
+import RxMoya
+import RxSwift
+
+
+extension MoyaProvider {
+	
+	// MARK: - Public methods
+	
+	func requestWithValidation(_ target: Target) -> Observable<Response> {
+		
+		return self.rx.request(target)
+			.asObservable()
+			.flatMap({ (response: Response) -> Observable<Response> in
+				
+				let responseSuccess: CountableRange<Int> = (200..<300)
+				
+				switch response.statusCode {
+					
+				case responseSuccess:
+					
+					return Observable<Response>.just(response)
+					
+				default:
+							
+					let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey:"Failed to parse server response"])
+					
+					return Observable<Response>.error(error)
+				}
+			})
+	}
+	
+}
