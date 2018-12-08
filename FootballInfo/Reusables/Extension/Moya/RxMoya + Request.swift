a9759a0306d1b2e1a3ca221d07c2108ecd76c3eb
Fathureza Januarza <fathureza.januarza@icehousecorp.com>
2018-12-08 21:29:02 +0700

    Add file for extension moya requestProvider and TeamListViewController

---
+++ FootballInfo/Reusables/Extension/Moya/RxMoya + Request.swift
@@ -0,0 +1,9 @@
+//
+//  RxMoya + Request.swift
+//  FootballInfo
+//
+//  Created by Fathureza Januarza on 08/12/18.
+//  Copyright © 2018 Fathureza Januarza. All rights reserved.
+//
+
+import Foundation
