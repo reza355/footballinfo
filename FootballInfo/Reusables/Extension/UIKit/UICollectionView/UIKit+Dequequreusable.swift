eb756183a2f61e6a7b4a6d2da6791c426a2be535
Fathureza Januarza <fathureza.januarza@icehousecorp.com>
2018-12-10 15:39:41 +0700

    Add new file

---
+++ FootballInfo/Reusables/Extension/UIKit/UICollectionView/UIKit+Dequequreusable.swift
@@ -0,0 +1,9 @@
+//
+//  UIKit.swift
+//  FootballInfo
+//
+//  Created by Fathureza Januarza on 10/12/18.
+//  Copyright © 2018 Fathureza Januarza. All rights reserved.
+//
+
+import Foundation
---
+++ FootballInfo/TeamList/Cells/TeamLogoCell.swift
@@ -0,0 +1,18 @@
+//
+//  TeamLogoCell.swift
+//  FootballInfo
+//
+//  Created by Fathureza Januarza on 10/12/18.
+//  Copyright © 2018 Fathureza Januarza. All rights reserved.
+//
+
+import UIKit
+
+class TeamLogoCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
