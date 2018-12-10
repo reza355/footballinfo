c9c258324b7f239ceaf005f0c4903ef36760ad4b
Fathureza Januarza <fathureza.januarza@icehousecorp.com>
2018-12-10 11:12:31 +0700

    Configure getTeamLists moya target

--- FootballInfo/TeamList/MoyaTarget/TeamListsMoyaTarget.swift
+++ FootballInfo/TeamList/MoyaTarget/TeamListsMoyaTarget.swift
@@ -7,3 +7,57 @@
 //
 
 import Foundation
+import Moya
+
+enum TeamListsMoyaTarget {
+	case getTeamLists
+}
+
+extension TeamListsMoyaTarget: TargetType {
+	
+	var baseURL: URL {
+		
+		if let validURL = URL(string: "https://www.thesportsdb.com/api/v1/json/1") {
+			return validURL
+		} else {
+			return NSURL() as URL
+		}
+	}
+	
+	var path: String {
+		switch self {
+		case .getTeamLists:
+			return "/search_all_leagues.php"
+		}
+	}
+	
+	var method: Moya.Method {
+		switch self {
+		case .getTeamLists:
+			return .get
+		}
+	}
+	
+	var parameterEncoding: ParameterEncoding {
+		return URLEncoding.default
+	}
+	
+	var parameters: [String: Any] {
+		switch self {
+		default:
+			return ["c":"England"]
+		}
+	}
+	
+	var task: Task {
+		return .requestParameters(parameters: parameters, encoding: parameterEncoding)
+	}
+	
+	var headers: [String : String]? {
+		return [:]
+	}
+	
+	var sampleData: Data {
+		return Data()
+	}
+}
