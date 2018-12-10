dd06e0f38fb395025ef3bf689adc673053af59b9
Fathureza Januarza <fathureza.januarza@icehousecorp.com>
2018-12-10 11:11:46 +0700

    Configure team lists cache mode

--- FootballInfo/TeamList/Models/TeamListsCacheModel.swift
+++ FootballInfo/TeamList/Models/TeamListsCacheModel.swift
@@ -7,3 +7,58 @@
 //
 
 import Foundation
+import RxSwift
+import ObjectMapper
+
+final class TeamListsCacheModel {
+	
+	// MARK: - Public Properties
+	
+	static let sharedInstance = TeamListsCacheModel()
+	
+	var currentFootballTeam = Variable<[FootballTeam]>([])
+	
+	// MARK: - Private Properties
+	
+	private static let footballTeamKey = "FootballTeam"
+	
+	private let userDefaults = UserDefaults.standard
+	
+	// MARK: - Public Methods
+	
+	func saveFootballTeamCache(footballTeam: [FootballTeam]) {
+		
+		guard let footballTeamObjectString = Mapper().toJSONString(footballTeam) else {
+			return
+		}
+		
+		currentFootballTeam.value = footballTeam
+		
+		userDefaults.set(footballTeamObjectString, forKey: TeamListsCacheModel.footballTeamKey)
+	}
+	
+	func clearCache() {
+		
+		currentFootballTeam.value = []
+		
+		userDefaults.removeObject(forKey: TeamListsCacheModel.footballTeamKey)
+	}
+	
+	// MARK: - Private Methods
+	
+	private init() {
+		
+		loadCachedFootballTeam()
+	}
+	
+	private func loadCachedFootballTeam() {
+		
+		guard let footballTeamObjectString = userDefaults.string(forKey: TeamListsCacheModel.footballTeamKey),
+			let footballTeam = Mapper<FootballTeam>().mapArray(JSONString: footballTeamObjectString) else {
+				
+				return
+		}
+		
+		currentFootballTeam.value = footballTeam
+	}
+}
